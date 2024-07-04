fb34a7ee3986db8837c162cfbfb8f39a3d978873
0xShaka <0xShaka@gmail.com>
2024-07-04 19:02:41 +0300

    Installed tokio and eyre as suggested in alloy docs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+//! Example of creating an HTTP provider using the `on_http` method on the `ProviderBuilder`.
+
+use alloy::providers::{Provider, ProviderBuilder};
+use eyre::Result;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    // ...
+
+    Ok(())
 }
